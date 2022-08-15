1af9f8db0827230f0bad7b0531160ab00fd7153e
yingml <yingml@honlife.com.cn>
2022-08-15 10:23:54 +0800

    项目初始化

---
+++ workflow-template/src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>hello {{ name }}, this is my first page.</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'peter',
+      phone: 18712345678
+    }
+  },
+  mounted() {
+    console.log('hey there')
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
