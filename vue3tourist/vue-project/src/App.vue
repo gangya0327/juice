65916d10bff1a945f690beb621d6b8a91ce2441e
gangya0327 <king9999427@163.com>
2022-10-28 09:35:24 +0800

    vue3基础学习

---
+++ vue3tourist/vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+
+const blog = ref({
+  title: "vue3 基础教程",
+  content: "vue3 的一些学习知识",
+  link: "/vue3-tourist",
+});
+</script>
+
+<template>
+  <h1>
+    <a :href="blog.link">{{ blog.title }}</a>
+  </h1>
+  <article>
+    <div>{{ blog.content }}</div>
+  </article>
+</template>
+
+<style scoped></style>
