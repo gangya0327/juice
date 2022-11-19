4aebb3a459e50cfd44dcd6369adba248a0344876
yingml <yingml@honlife.com.cn>
2022-11-20 03:49:33 +0800

    安装elementui，sass

--- jindu/project/project-one/src/App.vue
+++ jindu/project/project-one/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -16,7 +16,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url('./styles/reset.css');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- jindu/project/project-one/src/main.js
+++ jindu/project/project-one/src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// Vue.use(ElementUI)
+
+// import { Button, Switch } from 'element-ui'
+// // Vue.component(Button.name, Button)
+// // Vue.component(Switch.name, Switch)
+// Vue.use(Button)
+// Vue.use(Switch)
+
+import './plugins/element'
+
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App)
 }).$mount('#app')
