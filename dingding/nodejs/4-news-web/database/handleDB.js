ee66868e3a8324e3463adb8e244fff9bac4d8bb3
yingml <yingml@honlife.com.cn>
2023-01-30 17:48:00 +0800

    配置数据库连接

---
+++ dingding/nodejs/4-news-web/database/handleDB.js
@@ -0,0 +1,45 @@
+const db = require('./db-orm')
+async function handleDB(res, tableName, methodName, errorMsg, n1, n2) {
+  let Students = db.model(tableName)
+  let result
+  try {
+    result = await new Promise((resolve, reject) => {
+      // 没有参数
+      if (!n1) {
+        Students[methodName]((err, data) => {
+          if (err) {
+            reject(err)
+            return
+          }
+          resolve(data)
+        })
+        return
+      }
+      // 只传一个参数
+      if (!n2) {
+        Students[methodName](n1, (err, data) => {
+          if (err) {
+            reject(err)
+            return
+          }
+          resolve(data)
+        })
+        return
+      }
+      // 传了两个参数
+      Students[methodName](n1, n2, (err, data) => {
+        if (err) {
+          reject(err)
+          return
+        }
+        resolve(data)
+      })
+    })
+  } catch (error) {
+    res.send({ message: `${errorMsg} ${error.sqlMessage}`, code: 501 })
+    return
+  }
+  return result
+}
+
+module.exports = handleDB
--- dingding/nodejs/4-news-web/routes/index.js
+++ dingding/nodejs/4-news-web/routes/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const handleDB = require('../database/handleDB')
+
 const router = express.Router()
 
 router.get('/set_cookie', (req, res) => {
@@ -11,6 +13,14 @@ router.get('/get_cookie', (req, res) => {
   const name = req.cookies['name']
   const age = req.session['age']
   res.send(`获取cookie值为${name}，session值为${age}`)
+})
+
+// 测试数据库连接
+router.get('/get_data', (req, res) => {
+  ;(async function () {
+    const result = await handleDB(res, 'info_category', 'find', 'info_category查询出错')
+    res.send(result)
+  })()
 })
 
 router.get('/', (req, res) => {
