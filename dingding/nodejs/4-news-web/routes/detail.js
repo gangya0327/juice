22d82e4076f3562198d1878d3c8838f84252d68c
yingml <yingml@honlife.com.cn>
2023-01-31 22:32:32 +0800

    获取新闻详情，点击量

--- dingding/nodejs/4-news-web/routes/detail.js
+++ dingding/nodejs/4-news-web/routes/detail.js
@@ -4,7 +4,7 @@ require('../utils/filters')
 
 const router = express.Router()
 
-router.get('/news_detail/:id', (req, res) => {
+router.get('/news_detail/:news_id', (req, res) => {
   ;(async function () {
     const userId = req.session['USER_ID']
     // 获取数据库中id的用户信息
@@ -20,6 +20,11 @@ router.get('/news_detail/:id', (req, res) => {
       'info_news查询出错',
       'select title,id from info_news order by clicks desc limit 6'
     )
+    // 新闻详情
+    const { news_id } = req.params
+    const newsDetail = await handleDB(res, 'info_news', 'find', 'info_news查询出错', `id=${news_id}`)
+    const newClicks = newsDetail[0].clicks + 1
+    await handleDB(res, 'info_news', 'update', 'info_news修改出错', `id=${news_id}`, { clicks: newClicks })
     const data = {
       user_info: result[0]
         ? {
@@ -27,8 +32,10 @@ router.get('/news_detail/:id', (req, res) => {
             avatar_url: result[0].avatar_url
           }
         : false,
-      clickRanking
+      clickRanking,
+      newsDetail: newsDetail[0]
     }
+
     res.render('detail', data)
   })()
 })
--- dingding/nodejs/4-news-web/utils/filters.js
+++ dingding/nodejs/4-news-web/utils/filters.js
@@ -3,3 +3,20 @@ const template = require('art-template')
 template.defaults.imports.rankingFilter = (value) => {
   return ['first', 'second', 'third'][value] || ''
 }
+
+template.defaults.imports.dateFormat = (value) => {
+  const d = new Date(value)
+  return (
+    d.getFullYear() +
+    '-' +
+    (d.getMonth() + 1) +
+    '-' +
+    d.getDate() +
+    ' ' +
+    d.getHours() +
+    ':' +
+    d.getMinutes() +
+    ':' +
+    d.getSeconds()
+  )
+}
