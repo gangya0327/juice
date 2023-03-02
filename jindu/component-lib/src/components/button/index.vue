c52d1a81d970b12f288ec0d0ed27a1e46823a6cf
yingml <yingml@honlife.com.cn>
2023-03-02 20:33:47 +0800

    组件封装项目初始化

---
+++ jindu/component-lib/src/components/button/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AButton',
+  data() {
+    return {
+      a: '',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
