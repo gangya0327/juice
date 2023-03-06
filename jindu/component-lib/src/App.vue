94603460d34dfa58c669227bb80caf79f1eb2646
yingml <yingml@honlife.com.cn>
2023-03-06 16:57:25 +0800

    组件菜单

--- jindu/component-lib/src/App.vue
+++ jindu/component-lib/src/App.vue
@@ -1,5 +1,23 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="nav">
+      <router-link to="/">首页</router-link>
+      <router-link to="/button">Button</router-link>
+      <router-link to="/table">Table</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
+
+<style lang="scss" scoped>
+#app {
+  .nav {
+    margin: 20px 0;
+    a {
+      padding-right: 20px;
+      margin-right: 20px;
+      border-right: 1px solid;
+    }
+  }
+}
+</style>
--- jindu/component-lib/src/views/HomeView.vue
+++ jindu/component-lib/src/views/HomeView.vue
@@ -1,95 +1,11 @@
 <template>
-  <div class="home">
-    <div class="block">
-      <yzd-button>默认按钮</yzd-button>
-      <yzd-button type="primary">默认按钮</yzd-button>
-      <yzd-button type="success">成功按钮</yzd-button>
-      <yzd-button type="warning">警告按钮</yzd-button>
-      <yzd-button type="danger">成功按钮</yzd-button>
-    </div>
-    <div class="block">
-      <yzd-button border>默认按钮</yzd-button>
-      <yzd-button type="primary" border>默认按钮</yzd-button>
-      <yzd-button type="success" border>成功按钮</yzd-button>
-      <yzd-button type="warning" border>警告按钮</yzd-button>
-      <yzd-button type="danger" border>成功按钮</yzd-button>
-    </div>
-    <div class="block">
-      <yzd-button round>默认按钮</yzd-button>
-      <yzd-button type="primary" round>默认按钮</yzd-button>
-      <yzd-button type="success" round>成功按钮</yzd-button>
-      <yzd-button type="warning" round>警告按钮</yzd-button>
-      <yzd-button type="danger" round>成功按钮</yzd-button>
-    </div>
-    <div class="block">
-      <yzd-button disabled>默认按钮</yzd-button>
-      <yzd-button type="primary" disabled>默认按钮</yzd-button>
-      <yzd-button type="success" disabled>成功按钮</yzd-button>
-      <yzd-button type="warning" disabled>警告按钮</yzd-button>
-      <yzd-button type="danger" disabled>成功按钮</yzd-button>
-    </div>
-    <div class="block">
-      <yzd-button type="primary">默认尺寸</yzd-button>
-      <yzd-button type="primary" size="medium">中等尺寸</yzd-button>
-      <yzd-button type="primary" size="small">小尺寸</yzd-button>
-      <yzd-button type="primary" size="mini">超小尺寸</yzd-button>
-    </div>
-    <div class="block">
-      <yzd-button type="primary" min-width="160px">160px尺寸</yzd-button>
-      <yzd-button type="primary" min-width="300px">300px尺寸</yzd-button>
-    </div>
-    <div class="block">
-      <yzd-button type="primary" prefix="likefill"> 图标按钮 </yzd-button>
-      <yzd-button type="primary" suffix="forwardfill"> 图标按钮 </yzd-button>
-      <yzd-button type="success" size="small" border suffix="addressbook"> 图标按钮 </yzd-button>
-      <yzd-button type="warning" size="mini" border prefix="goods"> 图标按钮 </yzd-button>
-      <yzd-button type="danger" size="mini" border prefix="squarecheckfill"> 图标按钮 </yzd-button>
-    </div>
-    <div class="block">
-      <yzd-button type="primary" block>块级按钮 </yzd-button>
-    </div>
-    <div class="block">
-      <yzd-button type="primary" loading> 加载按钮 </yzd-button>
-      <yzd-button type="primary" :loading="flag" @click="handlerChange"> 点击主动加载 </yzd-button>
-      <yzd-button type="primary" :before-change="asyncFunction"> 点击异步加载 </yzd-button>
-    </div>
-    <div class="block">
-
-    </div>
+  <div>
+    <h3>首页</h3>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
-  components: {
-    'yzd-button': () => import('@/components/YzdButton/index.vue'),
-  },
-  data() {
-    return {
-      flag: false,
-    };
-  },
-  methods: {
-    handlerChange() {
-      this.flag = true;
-      setTimeout(() => {
-        this.flag = false;
-      }, 2000);
-    },
-    asyncFunction() {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve();
-        }, 2000);
-      });
-    },
-  },
 };
 </script>
-
-<style lang="scss" scoped>
-.block {
-  margin-bottom: 10px;
-}
-</style>
