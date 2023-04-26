30e0e7a7fe2201e2bd19c7edd06e27ebfbf6e81f
gangya0327 <king9999427@163.com>
2023-04-26 21:34:44 +0800

    feat: 创建server

--- xiaoyesensen/chat/server/app.js
+++ xiaoyesensen/chat/server/app.js
@@ -1,17 +1,17 @@
-const express = require('express');
 const { Server } = require('socket.io');
-
-const app = express();
-app.listen(8000, () => {
-  console.log('server is opening...');
-});
 
 const io = new Server(3000, {
   cors: {
-    origin: ['http://localhost:7070'],
+    origin: ['http://localhost:8080'],
   },
 });
 
 io.on('connection', (socket) => {
-  console.log('connected');
+  console.log('connected', socket);
+});
+
+const express = require('express');
+const app = express();
+app.listen(8000, () => {
+  console.log('server is opening on http://localhost:8000');
 });
