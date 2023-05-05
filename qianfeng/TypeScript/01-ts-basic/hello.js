3e504ece4ede5c53914968af0752a434f05fbdf1
gangya0327 <king9999427@163.com>
2023-05-05 21:30:53 +0800

    feat: ts基础使用

---
+++ qianfeng/TypeScript/01-ts-basic/hello.js
@@ -0,0 +1,4 @@
+function greet(person, date) {
+    console.log("hello ".concat(person, ", today is ").concat(date));
+}
+greet('peter', '202304012');
---
+++ qianfeng/TypeScript/01-ts-basic/hello.ts
@@ -0,0 +1,5 @@
+function greet(person: string, date: string) {
+  console.log(`hello ${person}, today is ${date}`);
+}
+
+greet('peter', '202304012');
