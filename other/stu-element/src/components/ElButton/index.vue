c7a92bddd372def62604c78003bd87b20a236912
gangya0327 <king9999427@163.com>
2023-06-19 20:00:03 +0800

    ✨ feat: 基础用法

---
+++ other/stu-element/src/components/ElButton/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <button
+    :disable="disabled"
+    :class="[
+      'el-button',
+      'el-button--' + (type || 'default'),
+      { 'is-plain': plain },
+      { 'is-round': round },
+      { 'is-circle': circle },
+      { 'is-disabled': disabled },
+    ]"
+  >
+    <i v-if="icon" :class="icon"></i>
+    <span v-if="$slots.default">
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ElButton',
+  props: {
+    type: {
+      type: String,
+    },
+    plain: {
+      type: Boolean,
+      default: false,
+    },
+    round: {
+      type: Boolean,
+      default: false,
+    },
+    circle: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: String,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  created() {
+    console.log(this.$slots);
+  },
+};
+</script>
