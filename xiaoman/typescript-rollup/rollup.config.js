fcbc06faa34ff788ece632b7f86e24d2092f9217
gangya0327 <king9999427@163.com>
2023-07-10 15:28:39 +0800

    ✨ feat: rollup ts 配置

--- xiaoman/typescript-rollup/rollup.config.js
+++ xiaoman/typescript-rollup/rollup.config.js
@@ -1,11 +1,34 @@
+console.log(process.env);
+
 import path from 'path';
 import ts from 'rollup-plugin-typescript2';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import replace from 'rollup-plugin-replace';
 
 export default {
   input: './src/index.ts',
   output: {
     file: path.resolve(__dirname, './lib/index.js'),
     format: 'umd',
+    sourceMap: true,
   },
-  plugins: [ts()],
+  plugins: [
+    ts(), // 编译ts语句
+    livereload(), // 热加载
+    terser({
+      compress: {
+        drop_console: true, // 删除console语句
+      },
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    serve({
+      open: false,
+      port: 7001,
+      openPage: '/public/index.html',
+    }),
+  ],
 };
--- xiaoman/typescript-rollup/src/index.ts
+++ xiaoman/typescript-rollup/src/index.ts
@@ -1,2 +1,11 @@
-const str: string = 'abc';
+const str: string = 'abc123456';
+
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('env: development');
+} else {
+  console.log('env: production');
+}
+
 console.log(str);
