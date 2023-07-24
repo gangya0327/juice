786a9d6c3cbff37b7a33d262ad0ec5a4dafa5256
gangya0327 <king9999427@163.com>
2023-07-24 09:28:03 +0800

    java demo

---
+++ aiguigu/java/demo1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author 应孟霖
+ * @version 1.1
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 这是输出语句
+        System.out.println("hello world，中国");
+    }
+}
