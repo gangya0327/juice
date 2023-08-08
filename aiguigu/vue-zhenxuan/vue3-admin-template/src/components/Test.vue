c87a67ebf44eb86a8db1f8a7a165302b4158f93b
gangya0327 <king9999427@163.com>
2023-08-08 17:42:07 +0800

    ✨ feat: 移除eslint，设置环境变量

---
+++ aiguigu/vue-zhenxuan/vue3-admin-template/src/components/Test.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Test</h2>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="primary" :icon="Edit">Primary</el-button>
+
+    <el-pagination
+      v-model:current-page="currentPage1"
+      :page-size="100"
+      :small="small"
+      :disabled="disabled"
+      :background="background"
+      layout="total, prev, pager, next"
+      :total="1000"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Edit } from '@element-plus/icons-vue';
+import { ref } from 'vue';
+
+const currentPage1 = ref(5);
+const small = ref(false)
+const background = ref(false);
+const disabled = ref(false);
+
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`);
+};
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`);
+};
+
+console.log('test', import.meta.env);
+</script>
+
+<style lang="scss" scope></style>
--- aiguigu/vue-zhenxuan/vue3-admin-template/src/main.ts
+++ aiguigu/vue-zhenxuan/vue3-admin-template/src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+console.log('main', import.meta.env);
+
+
+app.use(ElementPlus, {
+  locale: zhCn
+})
+app.mount('#app')
