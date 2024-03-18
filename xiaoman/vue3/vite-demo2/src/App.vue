29fa91e72bbbe426ae5783447492725dcb28c38b
gangya0327 <king9999427@163.com>
2024-03-18 09:48:27 +0800

    feat：基础知识

---
+++ xiaoman/vue3/vite-demo2/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>{{ Person }}</div>
+    <button @click="change">修改</button>
+  </div>
+</template>
+
+<script setup>
+import { isRef, ref } from 'vue';
+
+const Person = ref({ name: 'yml' });
+
+const change = () => {
+  Person.value.name = 'peter';
+  console.log(isRef(Person));
+};
+</script>
