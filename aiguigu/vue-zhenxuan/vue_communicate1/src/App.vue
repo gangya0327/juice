1a6b7bb54bca8a201676559cffa7a72e2bdbe210
gangya0327 <king9999427@163.com>
2024-03-22 18:44:30 +0800

    props

--- aiguigu/vue-zhenxuan/vue_communicate1/src/App.vue
+++ aiguigu/vue-zhenxuan/vue_communicate1/src/App.vue
@@ -8,11 +8,10 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/props">Props</RouterLink>
       </nav>
     </div>
   </header>
@@ -32,7 +31,7 @@ header {
 }
 
 nav {
-  width: 100%;
+  width: 300px;
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
---
+++ aiguigu/vue-zhenxuan/vue_communicate1/src/views/1_props/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Child from './Child.vue';
+</script>
+
+<template>
+  <div class="box">
+    <p>我是父组件</p>
+    <Child info="some message"></Child>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  background-color: #ccc;
+}
+</style>
