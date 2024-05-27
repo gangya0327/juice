8e0fb8557d4cb58ed7662a35878c9d121989d529
gangya0327 <king9999427@163.com>
2024-05-27 13:50:13 +0800

    ✨ feat: node脚手架开发

---
+++ other/node-cli/template/commitlint.config.js
@@ -0,0 +1,4 @@
+// module.exports = { extends: ['@commitlint/config-conventional'] }
+
+// 自定义规范
+module.exports = { extends: ['cz'] }
---
+++ other/node-cli/template/src/views/home/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div>hello {{ name }}, this is my first page.</div>
+    <div>
+      i like
+      <div
+        name="fruitname"
+        :key="f"
+        ref="f"
+        class="fruitclass"
+        v-for="f in fruits"
+        id="fruitid">
+        <span>some {{ f }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'peter',
+      phone: 18712345678,
+      fruits: ['apple', 'banana', 'orange', 'peach']
+    }
+  },
+  mounted() {
+    console.log('hey there')
+  },
+  created() {
+    console.log(123)
+    const fn = x => {
+      console.log(x)
+    }
+    fn(1, 2)
+    this.somefn().then(res => {
+      console.log('res :>> ', res)
+    })
+  },
+  methods: {
+    somefn() {
+      console.log('somefn')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
