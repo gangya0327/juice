08ebeed4dc95945f8241049e7f5c10b19c82b3ee
gangya0327 <king9999427@163.com>
2024-11-16 22:45:28 +0800

    大师课step1

---
+++ duyi/dashike-3days/my-vue-step2/effect.js
@@ -0,0 +1,9 @@
+// 依赖收集
+export function track(target, key) {
+  console.log(`%c依赖收集：${key}`, 'color: #f00');
+}
+
+// 派发更新
+export function trigger(target, key) {
+  console.log(`%c派发更新：${key}`, 'color: #00f');
+}
---
+++ duyi/dashike-3days/my-vue-step2/reactive.js
@@ -0,0 +1,17 @@
+import { track, trigger } from './effect.js';
+
+export function reactive(target) {
+  return new Proxy(target, {
+    get(target, key) {
+      // 依赖收集
+      track(target, key);
+      return target[key]; // 返回对象的相应属性值
+    },
+    set(target, key, value) {
+      // 派发更新
+      trigger(target, key);
+      Reflect.set(target, key, value); // 设置对象的相应属性值
+      return true;
+    },
+  });
+}
